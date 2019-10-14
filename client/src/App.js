18ede3412678ce279b1db9467a3e4e1e41f91370
Liam Dawson <liamrdawson@gmail.com>
2019-10-14 20:22:39 +0100

    clean up react files and add bootstrap for quick prototyping

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 
 const App = () => (
-  <div className="App">
-    app
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react"></i> React and Express File Uploader
+    </h4>
   </div>
 );
 
