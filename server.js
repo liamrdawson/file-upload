64e5ec346f78bd6605fcbf6972ed5eaa9012f9a9
Liam Dawson <liamrdawson@gmail.com>
2019-10-14 19:56:36 +0100

    setup Express server in server.js

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.post('/upload', (req, res) => {
     if (req.files === null) {
         return res.status(400).json({msg: 'No file was uploaded'})
     } 
+
+    const file = req.files.file
+    file.mv(`${__dirname}/client/public/uploads/${filename}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        res.json({ 
+            filename: file.name,
+            filePath: `/uploads/${file.name}` });
+    })
+
 });
 
 app.listen(5000, () => console.log('Server started...'));
